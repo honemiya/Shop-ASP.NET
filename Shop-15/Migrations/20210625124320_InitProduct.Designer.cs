e65c36838748b6dd887e6aeeadeeb7b413e4dd2c
HennessyZazzz <60851577+HennessyZazzz@users.noreply.github.com>
2021-06-25 15:47:33 +0300

    bags

--- Shop-15/Migrations/20210625124320_InitProduct.Designer.cs
+++ Shop-15/Migrations/20210625124320_InitProduct.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shop_15.Data;
 
 namespace Shop_15.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210625124320_InitProduct")]
+    partial class InitProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shop-15/Models/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_15.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string ShortDest { get; set; }
+        public string Desecription { get; set; }
+        [Range(1, int.MaxValue)]
+        public double Price { get; set; }
+        public string Image { get; set; }
+        [Display(Name = "Category Type")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
